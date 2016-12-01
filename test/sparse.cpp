b802bf05c5770f3e20cf523d6fdf5269df8b34de
Shehzan Mohammed <shehzan@arrayfire.com>
2016-12-01 11:39:07 -0500

    Add tests for sparse arithmetic

--- test/sparse.cpp
+++ test/sparse.cpp
@@ -381,3 +381,179 @@ CAST_TESTS(cfloat , cdouble )
 
 CAST_TESTS(cdouble, cfloat  )
 CAST_TESTS(cdouble, cdouble )
+
+
+typedef enum {
+    af_add_t,
+    af_sub_t,
+    af_mul_t,
+    af_div_t,
+} af_op_t;
+
+template<af_op_t op>
+struct arith_op
+{
+    af::array operator()(af::array v1, af::array v2)
+    {
+        return v1;
+    }
+};
+
+template<>
+struct arith_op<af_add_t>
+{
+    af::array operator()(af::array v1, af::array v2)
+    {
+        return v1 + v2;
+    }
+};
+
+template<>
+struct arith_op<af_sub_t>
+{
+    af::array operator()(af::array v1, af::array v2)
+    {
+        return v1 - v2;
+    }
+};
+
+template<>
+struct arith_op<af_mul_t>
+{
+    af::array operator()(af::array v1, af::array v2)
+    {
+        return v1 * v2;
+    }
+};
+
+template<>
+struct arith_op<af_div_t>
+{
+    af::array operator()(af::array v1, af::array v2)
+    {
+        return v1 / v2;
+    }
+};
+
+template<typename T, af_op_t op>
+void sparseArithTester(const int m, const int n, int factor, const double eps)
+{
+    af::deviceGC();
+
+    if (noDoubleTests<T>()) return;
+
+#if 1
+    af::array A = cpu_randu<T>(af::dim4(m, n));
+    af::array B = cpu_randu<T>(af::dim4(m, n));
+#else
+    af::array A = af::randu(m, n, (af::dtype)af::dtype_traits<T>::af_type);
+    af::array B = af::randu(m, n, (af::dtype)af::dtype_traits<T>::af_type);
+#endif
+
+    A = makeSparse<T>(A, factor);
+
+    af::array SA = af::sparse(A, AF_STORAGE_CSR);
+    af::array OA = af::sparse(A, AF_STORAGE_COO);
+
+    // Arith Op
+    af::array resS = arith_op<op>()(SA, B);
+    af::array resO = arith_op<op>()(OA, B);
+    af::array resD = arith_op<op>()( A, B);
+
+    af::array revS = arith_op<op>()(B, SA);
+    af::array revO = arith_op<op>()(B, OA);
+    af::array revD = arith_op<op>()(B,  A);
+
+    ASSERT_NEAR(0, af::sum<double>(af::abs(real(resS - resD))) / (m * n), eps);
+    ASSERT_NEAR(0, af::sum<double>(af::abs(imag(resS - resD))) / (m * n), eps);
+
+    ASSERT_NEAR(0, af::sum<double>(af::abs(real(resO - resD))) / (m * n), eps);
+    ASSERT_NEAR(0, af::sum<double>(af::abs(imag(resO - resD))) / (m * n), eps);
+
+    ASSERT_NEAR(0, af::sum<double>(af::abs(real(revS - revD))) / (m * n), eps);
+    ASSERT_NEAR(0, af::sum<double>(af::abs(imag(revS - revD))) / (m * n), eps);
+
+    ASSERT_NEAR(0, af::sum<double>(af::abs(real(revO - revD))) / (m * n), eps);
+    ASSERT_NEAR(0, af::sum<double>(af::abs(imag(revO - revD))) / (m * n), eps);
+}
+
+template<typename T>
+void sparseArithTesterDiv(const int m, const int n, int factor, const double eps)
+{
+    af::deviceGC();
+
+    if (noDoubleTests<T>()) return;
+
+#if 1
+    af::array A = cpu_randu<T>(af::dim4(m, n));
+    af::array B = cpu_randu<T>(af::dim4(m, n));
+#else
+    af::array A = af::randu(m, n, (af::dtype)af::dtype_traits<T>::af_type);
+    af::array B = af::randu(m, n, (af::dtype)af::dtype_traits<T>::af_type);
+#endif
+
+    A = makeSparse<T>(A, factor);
+
+    af::array SA = af::sparse(A, AF_STORAGE_CSR);
+    af::array OA = af::sparse(A, AF_STORAGE_COO);
+
+    // Arith Op
+    af::array resS = arith_op<af_div_t>()(SA, B);
+    af::array resO = arith_op<af_div_t>()(OA, B);
+    af::array resD = arith_op<af_div_t>()( A, B);
+
+    af::array revS = arith_op<af_div_t>()(B, SA);
+    af::array revO = arith_op<af_div_t>()(B, OA);
+    af::array revD = arith_op<af_div_t>()(B,  A);
+
+    T *hResS = resS.host<T>();
+    T *hResO = resO.host<T>();
+    T *hResD = resD.host<T>();
+    T *hRevS = revS.host<T>();
+    T *hRevO = revO.host<T>();
+    T *hRevD = revD.host<T>();
+
+    for(int i = 0; i < B.elements(); i++) {
+        ASSERT_EQ(hResS[i], hResD[i]) << "at : " << i;
+        ASSERT_EQ(hResO[i], hResD[i]) << "at : " << i;
+        ASSERT_EQ(hRevS[i], hRevD[i]) << "at : " << i;
+        ASSERT_EQ(hRevO[i], hRevD[i]) << "at : " << i;
+    }
+
+    af::freeHost(hResS);
+    af::freeHost(hResO);
+    af::freeHost(hResD);
+    af::freeHost(hRevS);
+    af::freeHost(hRevO);
+    af::freeHost(hRevD);
+}
+
+#define ARITH_TESTS_OPS(T, M, N, F, EPS)                                    \
+    TEST(SPARSE_ARITH, T##_ADD_##M##_##N)                                   \
+    {                                                                       \
+        sparseArithTester<T, af_add_t>(M, N, F, EPS);                       \
+    }                                                                       \
+    TEST(SPARSE_ARITH, T##_SUB_##M##_##N)                                   \
+    {                                                                       \
+        sparseArithTester<T, af_sub_t>(M, N, F, EPS);                       \
+    }                                                                       \
+    TEST(SPARSE_ARITH, T##_MUL_##M##_##N)                                   \
+    {                                                                       \
+        sparseArithTester<T, af_mul_t>(M, N, F, EPS);                       \
+    }                                                                       \
+    TEST(SPARSE_ARITH, T##_DIV_##M##_##N)                                   \
+    {                                                                       \
+        sparseArithTesterDiv<T>(M, N, F, EPS);                              \
+    }                                                                       \
+
+#define ARITH_TESTS(T)                                                      \
+    ARITH_TESTS_OPS(T, 10  , 10  , 5, 1e-6)                                 \
+    ARITH_TESTS_OPS(T, 1024, 1024, 5, 1e-6)                                 \
+    ARITH_TESTS_OPS(T, 100 , 100 , 1, 1e-6)                                 \
+    ARITH_TESTS_OPS(T, 2048, 1000, 6, 1e-6)                                 \
+    ARITH_TESTS_OPS(T, 123 , 278 , 5, 1e-6)                                 \
+
+ARITH_TESTS(float  )
+ARITH_TESTS(double )
+ARITH_TESTS(cfloat )
+ARITH_TESTS(cdouble)
